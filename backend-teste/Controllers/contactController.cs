9497dd8304741a24279bdd6b3e92dc54f97c7b07
gi-cardoso <geovana.cardososilv@gmail.com>
2026-01-20 15:20:35 -0300

    chore: initial commit, project structure

---
+++ backend-teste/Controllers/contactController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace backend_teste.Controllers
+{
+    [Route("[controller]")]
+    public class contactController : Controller
+    {
+
+    }
+}
